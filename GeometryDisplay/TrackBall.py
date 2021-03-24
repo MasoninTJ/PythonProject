b3cdbab1057f70a24f4540926f736e63d0999096
Zeros遇见 <zeros_zhang@163.com>
2021-03-24 23:18:59 +0800

    绘制球函数，存在问题，明天调

--- GeometryDisplay/TrackBall.py
+++ GeometryDisplay/TrackBall.py
@@ -1,4 +1,5 @@
-from Geometry3D.Class3D import Point3D,Vector3D,Circle,Sphere
+import numpy as np
+from OpenGL.GL import *
 
 
 class TrackBall:
@@ -6,3 +7,23 @@ class TrackBall:
         pass
 
 
+def draw_sphere_coin():
+    # 绘制轨迹球的动画
+    m_sphere_radius = 0.3
+    draw_circle(m_sphere_radius, m_color=(1, 0, 0, 0))
+    glRotatef(np.pi/2, 1, 0, 0)
+    draw_circle(m_sphere_radius, m_color=(0, 1, 0, 0))
+    glRotatef(np.pi / 2, 0, 1, 0)
+    draw_circle(m_sphere_radius, m_color=(0, 0, 1, 0))
+    glLoadIdentity()
+
+
+def draw_circle(m_radius=1.0, m_point_num=100, m_color=(0, 0, 1, 0)):
+    # 在(0,0)原点位置画一个圆
+    glColor4f(*m_color)
+    glBegin(GL_LINE_LOOP)
+    for index in range(m_point_num):
+        index_angle = 2 * np.pi * index / m_point_num
+        glVertex2f(m_radius * np.cos(index_angle), m_radius * np.sin(index_angle))
+    glEnd()
+    glFlush()
