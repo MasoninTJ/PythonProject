15a75fbf485a910d05206a80157ef46eb58e844b
Zeros遇见 <zeros_zhang@163.com>
2021-03-30 21:43:27 +0800

    完成了一半，点击鼠标会对模型进行旋转，但是不是我需要的效果

--- PyTorch_Learning/04_回归问题实战/example_01.py
+++ PyTorch_Learning/04_回归问题实战/example_01.py
@@ -1,51 +1,31 @@
 import numpy as np
 
 
-def loss_to_w(curr_w, curr_b, x, y):
-    return 2 * x * (curr_w * x + curr_b - y)
+def gradient_w(curr_w, curr_b, points):
+    result = 2 * points[:, 0] * (curr_w * points[:, 0] + curr_b - points[:, 1])
+    return np.mean(result)
 
 
-def loss_to_b(curr_w, curr_b, x, y):
-    return 2 * (curr_w * x + curr_b - y)
+def gradient_b(curr_w, curr_b, points):
+    result = 2 * (curr_w * points[:, 0] + curr_b - points[:, 1])
+    return np.mean(result)
 
 
-def read_data():
-    points = np.genfromtxt("data.csv", delimiter=",")
-    return points
+def error(curr_w, curr_b, points):
+    result = (curr_w * points[:, 0] + curr_b - points[:, 1]) ** 2
+    return np.mean(result)
 
 
-def error(w, b, points):
-    list_error = []
-    for i in range(len(points)):
-        x = points[i, 0]
-        y = points[i, 1]
-        list_error.append((y - w * x - b) ** 2)
-    return sum(list_error) / len(points)
+test_points = np.genfromtxt("data.csv", delimiter=",")
 
+init_w = 0
+init_b = 0
+learning_rate = 0.0001
+points_count = len(test_points)
 
-def run():
-    init_w = 0
-    init_b = 0
-    learning_rate = 0.0001
-    points = read_data()
-    points_count = len(points)
-
-    b = init_b
-    w = init_w
-    for i in range(1000):
-        b_gradient_list = []
-        w_gradient_list = []
-        for j in range(points_count):
-            x = points[j, 0]
-            y = points[j, 1]
-            w_gradient_list.append(loss_to_w(w, b, x, y))
-            b_gradient_list.append(loss_to_b(w, b, x, y))
-        w_gradient = sum(w_gradient_list) / points_count
-        b_gradient = sum(b_gradient_list) / points_count
-        w -= (w_gradient * learning_rate)
-        b -= (b_gradient * learning_rate)
-        print(f'w={w}, b={b}, error={error(w, b, points)}')
-
-
-if __name__ == '__main__':
-    run()
+b = init_b
+w = init_w
+for i in range(1000):
+    w -= (gradient_w(w, b, test_points) * learning_rate)
+    b -= (gradient_b(w, b, test_points) * learning_rate)
+    print(f'w={w}, b={b}, error={error(w, b, test_points)}')
